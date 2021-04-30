b002e4c72c26874a6b8846f8be66febc02bbd007
vvechan <55418581+vvechan@users.noreply.github.com>
2021-04-30 15:45:01 +0800

    Update NoticeTemplate.py

--- NoticeTemplate.py
+++ NoticeTemplate.py
@@ -1,11 +1,12 @@
 # 普通模板
 def normalTemplate():
-    content = """{content}---
-数据日期:{date}
-今日总收益:{total_today}
-总可用积分:{avail_today}
-绑定账户:{account}
-设备总数:{devicesCount}
+    #content = """{content}---
+    content = """    
+数据日期:{date}\n
+今日总收益:{total_today}\n
+总可用积分:{avail_today}\n
+绑定账户:{account}\n
+设备总数:{devicesCount}\n
 **设备信息如下:**
 {detail}"""
     return content
